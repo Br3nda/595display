8e26a36c53c810bcca020e9cb33a3fce1e46ff24
Brenda Wallace <brenda@wallace.net.nz>
2016-07-13 21:15:40 +1200

    found on display at startup
    Signed-off-by: Brenda Wallace <brenda@wallace.net.nz>

--- examples/Display595/Display595.ino
+++ examples/Display595/Display595.ino
@@ -7,19 +7,14 @@ const int DISPLAY_DIGIT_COUNT = 8;
 
 Display595 display;
 
+
 void setup() {
 
-	Serial.begin(9600);
+  display.initialize(CLOCK_PIN, DATA_PIN, LATCH_PIN, DISPLAY_DIGIT_COUNT);
 
-	display.initialize(CLOCK_PIN, DATA_PIN, LATCH_PIN, DISPLAY_DIGIT_COUNT);
-
-	display.setNumberAndPoint(0, 7, false);
-	display.setNumberAndPoint(1, 0, false);
-	display.setNumberAndPoint(2, 8, false);
-	display.setNumberAndPoint(3, 3, false);
-	display.setNumberAndPoint(4, 1, false);
-	display.setNumberAndPoint(5, 7, false);
-	display.setNumberAndPoint(6, 0, false);
+  for(int i=0; i<DISPLAY_DIGIT_COUNT; i++) {
+    display.setNumberAndPoint(i, i, false);
+  }
 }
 
 void loop() {
